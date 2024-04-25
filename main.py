d885961ebb320d2dbdecc3979cf72fa0ef1f9851
bjorneme <bjorn.melaaen@gmail.com>
2024-04-25 16:33:44 +0200

    feat: Create config.json

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import json
 from stacked_mnist_tf import DataMode
 from system import System
 
-
 if __name__ == "__main__":
+    # Load configuration from JSON file
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+
+    # Initialize the System with parameters from the config
     system = System(
-        model_type='AE',
-        data_mode=DataMode.COLOR_BINARY_COMPLETE,
-        latent_dimension=64
+        model_type=config['model_type'],
+        data_mode=getattr(DataMode, config['data_mode']),
+        latent_dimension=config['latent_dimension']
     )
-    system.train(epochs=1)
-    system.evaluation()
+
+    # Train and evaluate the model
+    system.train(epochs=config['epochs'])
+
+    # AE-Basic
+    if config['task_type'] == 'AE-BASIC':
+        system.evaluation()
+
+    # AE-GEN
+    if config['task_type'] == 'AE-GEN':
+        system.generate_images()
+
+    # AE-ANOM
+    if config['task_type'] == 'AE-ANOM' and config['model_type'] == 'AE':
+        system.anomaly_detection_ae()
